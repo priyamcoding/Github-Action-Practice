acec267539c88fdcee2a60af01310d56d51f703d
priyamcoding <priyammahajan17@gmail.com>
2022-09-28 19:05:08 +0530

    Resolving error

--- add.py
+++ add.py
@@ -1,11 +1,12 @@
 
 import sys
+    #This method isnt working and my workflow is showing Index out of bounds error
 
 num1 = int(sys.argv[1])
 num2 = int(sys.argv[2])
 
-# num1 = int(input("submit num1"))
-# num2 = int(input("submit num2"))
+# num1 = 50
+# num2 = 7
 
 sum = num1 + num2
 
