06879397c648d4d6983348921f001f23e2329ba4
priyamcoding <priyammahajan17@gmail.com>
2022-09-28 18:52:39 +0530

    chnaged variable inputting method

--- add.py
+++ add.py
@@ -1,11 +1,11 @@
 
 import sys
 
-# num1 = int(sys.argv[1])
-# num2 = int(sys.argv[2])
+num1 = int(sys.argv[1])
+num2 = int(sys.argv[2])
 
-num1 = int(input("submit num1"))
-num2 = int(input("submit num2"))
+# num1 = int(input("submit num1"))
+# num2 = int(input("submit num2"))
 
 sum = num1 + num2
 
